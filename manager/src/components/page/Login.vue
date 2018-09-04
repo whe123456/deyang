09627905a4d3d23173588eb14f968b23a5403fef
hepeng <965197905@qq.com>
2018-09-04 13:25:59 +0800

    后管修改

--- manager/src/components/page/Login.vue
+++ manager/src/components/page/Login.vue
@@ -40,9 +40,9 @@ export default {
       const self = this
       self.$refs[formName].validate((valid) => {
         if (valid) {
-          localStorage.setItem('url', 'http://127.0.0.1:8180/')
+          // localStorage.setItem('url', 'http://127.0.0.1:8180/')
           // localStorage.setItem('url', 'http://192.168.0.188:8880/')
-          // localStorage.setItem('url', 'http://xs.17189.net/')
+          localStorage.setItem('url', 'http://xs.17189.net/')
           const url = localStorage.getItem('url')
           self.axios.get(url + 'api/api_login.php', { username: self.ruleForm.username, password: self.ruleForm.password }, function (res) {
             if (res.state === 'true') {
